655a6342090a29f2cd77209d2f481a475d40fe9c
Ozgur Ustun <ozgurustun65@gmail.com>
2020-05-09 11:25:41 +0300

    initial commit

---
+++ src/test/java/org/ebuy/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ebuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
